45d5c178a8d4acc24d8b86a1ca8ef5a8fbaab4d6
Denis Wolter <wolter.important@gmail.com>
2024-11-26 14:46:25 +0300

    add task methods of arrayObject---splice() and slice().js

---
+++ methods of arrayObject---splice() and slice().js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/572cb264362806af46000793/
+
+function threeInOne(arr){
+    const arr1 = [];
+    for (let i = 0; i <= arr.length; i += 3){
+      arr1.push(arr.slice(i, i + 3))
+    }
+    return arr1.map(v => v.reduce((a, b) => a + b,0)).slice(0,-1)
+}
