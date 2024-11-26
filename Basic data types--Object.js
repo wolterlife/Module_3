3740386a3313857c3b35c3b4269ff90ea0bf6541
Denis Wolter <wolter.important@gmail.com>
2024-11-26 14:36:15 +0300

    add task Training Time.js

--- Basic data types--Object.js
+++ Basic data types--Object.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/571f1eb77e8954a812000837/
 
 function animal(obj) {
-    return `This ${obj.color} ${obj.name} has ${obj.legs} legs`
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
 }
   
 
