126c066bd4138ac1866dd8e6a73d028dcbef71f9
Denis Wolter <wolter.important@gmail.com>
2024-11-26 15:57:07 +0300

    add task methods of arrayObject---filter().js

---
+++ methods of arrayObject---filter().js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/573023c81add650b84000429/train/javascript
+
+function countGrade(scores) {
+    return {
+      S: scores.filter(score => score === 100).length,
+      A: scores.filter(score => score < 100 && score >= 90).length,
+      B: scores.filter(score => score < 90 && score >= 80).length,
+      C: scores.filter(score => score < 80 && score >= 60).length,
+      D: scores.filter(score => score < 60 && score >= 0).length,
+      X: scores.filter(score => score === -1).length
+    };
+}
+  
