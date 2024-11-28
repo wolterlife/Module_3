7c7dd2960c496741fa5f75fa5b849b8447e2a25a
Denis Wolter <wolter.important@gmail.com>
2024-11-29 02:09:07 +0300

    Refactored Greeting.js

---
+++ cw/Refactored Greeting.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5121303128ef4b495f000001/
+
+class Person {
+    constructor(name) {
+      this.name = name
+    }
+  
+    greet(yourName) {
+      return `Hello ${yourName}, my name is ${this.name}`
+    }
+  }
