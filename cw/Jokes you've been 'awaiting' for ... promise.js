91dcc15efa823052f5448d8aecfd60fade94ba1c
Denis Wolter <wolter.important@gmail.com>
2024-11-28 15:18:25 +0300

    Jokes you've been 'awaiting' for ... promise.js

---
+++ cw/Jokes you've been 'awaiting' for ... promise.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5a353a478f27f244a1000076
+
+async function sayJoke(apiUrl, jokeId) {
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+    
+    if (!json.hasOwnProperty('jokes')) throw new Error(`No jokes at url: ${apiUrl}`)
+
+    const joke = json.jokes.find(function(joke) {
+      return joke.id === jokeId
+    })
+
+    if (!joke) throw new Error(`No jokes found id: ${jokeId}`)
+
+    return {
+      saySetup: () => joke.setup,
+      sayPunchLine: () => joke.punchLine,
+    }
+}
