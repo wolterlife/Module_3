8fc3d36b597a2eb3e3c583ef7c4c73928a7d4300
Denis Wolter <wolter.important@gmail.com>
2024-11-29 02:12:19 +0300

    Building blocks.js

---
+++ cw/Building blocks.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/55b75fcf67e558d3750000a3/train/javascript
+
+class Block {
+    constructor(data){
+      [this.w, this.l, this.h] = data;
+    }
+    getWidth(){ return this.w }
+    getLength(){ return this.l }
+    getHeight(){ return this.h }
+    getVolume(){
+      let {l, w, h} = this;
+      return w * l * h;
+    }
+    getSurfaceArea(){
+      let {l, w, h} = this;
+      return 2 * (l * w + l * h + w * h);
+    }
+}
