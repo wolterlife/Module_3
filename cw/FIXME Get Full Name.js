cb66b696e3528cc13283a787a9eaf7390044d3dc
Denis Wolter <wolter.important@gmail.com>
2024-11-30 18:43:02 +0300

    FIXME Get Full Name.js

---
+++ cw/FIXME Get Full Name.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/597c684822bc9388f600010f
+
+class Dinglemouse {
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    getFullName() {
+      return [this.firstName, this.lastName].filter(Boolean).join(' ');
+    }
+}
