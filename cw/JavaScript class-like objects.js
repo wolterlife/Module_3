a4999cf6be46832498739c7fbb9d15df9d191eeb
Denis Wolter <wolter.important@gmail.com>
2024-12-01 02:06:18 +0300

    JavaScript class-like objects.js

---
+++ cw/JavaScript class-like objects.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/513e1e47c600c93cef000001/
+
+function Animal(name, type) {
+    this.name = name; 
+    this.type = type; 
+}
+
+Animal.prototype.toString = function() {
+    return `${this.name} is a ${this.type}`;
+};
