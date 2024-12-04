ba57fae7f1df0d770f03682bb798f1bf386ea699
Denis Wolter <wolter.important@gmail.com>
2024-12-04 16:01:42 +0300

    Generating Generators - Generators #3.js

---
+++ cw/Generating Generators - Generators #3.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/56390ac2815fb1222a000070/
+
+function* multiplicationGenerator(n) {
+    for (let i = 1; i <= 10; i++) {
+      yield `${n} x ${i} = ${n * i}`;
+    }
+  }
+  
+  function* generator(a, b) {
+    for (let i = a; i <= b; i++) {
+      yield multiplicationGenerator(i);
+    }
+  }
