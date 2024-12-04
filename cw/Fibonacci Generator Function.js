96f179a365055a2d2a05f8cec67a02da3849d963
Denis Wolter <wolter.important@gmail.com>
2024-12-04 15:52:57 +0300

    Fibonacci Generator Function.js

---
+++ cw/Fibonacci Generator Function.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/58a5d9aaa49f6cd0f9000ad7/
+
+function *fibonacci() {
+    let a = 0, b = 1;
+    
+      while (true) {
+        yield a;
+    
+        const next = a + b;
+        a = b;
+        b = next;
+      }
+}
