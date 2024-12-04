882a8eff3f868c1d370fcff8659c6a076bc26184
Denis Wolter <wolter.important@gmail.com>
2024-12-04 19:17:19 +0300

    The Enigma Machine - Part 1 The Plugboard.js

---
+++ cw/The Enigma Machine - Part 1 The Plugboard.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5523b97ac8f5025c45000900/
+
+Plugboard = function(wires) {
+    wires = wires || '';
+  
+    if (!wires.match(/^([A-Z][A-Z]){0,10}$/)) 
+      throw Error();
+      
+    if (wires.match(/(.).*\1/)) 
+      throw Error();
+  
+    this.process = function(wire) {
+      var i = wires.indexOf(wire);
+      return i == -1 ? wire : wires[i + 1 - 2 * (i % 2)];
+    }
+  }
