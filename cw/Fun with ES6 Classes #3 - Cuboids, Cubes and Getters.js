91602a7e56b9e9a901d125dac4582a1bbb189843
Denis Wolter <wolter.important@gmail.com>
2024-12-04 18:44:27 +0300

    Fun with ES6 Classes #3 - Cuboids, Cubes and Getters.js

---
+++ cw/Fun with ES6 Classes #3 - Cuboids, Cubes and Getters.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/56fbdda707cff41b68000de2/
+
+class Cuboid {
+    constructor(length, width, height) {
+      this.length = length;
+      this.width = width;
+      this.height = height;
+    }
+  
+    get surfaceArea() {
+      return 2 * (this.length * this.width + this.width * this.height + this.height * this.length);
+    }
+  
+    get volume() {
+      return this.length * this.width * this.height;
+    }
+  }
+  
+  class Cube extends Cuboid {
+    constructor(length) {
+      super(length, length, length);
+    }
+  }
+  
