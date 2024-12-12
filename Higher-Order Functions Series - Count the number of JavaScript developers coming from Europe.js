f234d0acf7c21456ac58cdffaddccfe87e7c447a
Denis Wolter <wolter.important@gmail.com>
2024-12-13 00:49:36 +0300

    Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe.js

---
+++ Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe.js
@@ -0,0 +1,10 @@
+function countDevelopers(list) {
+    return list.filter(el => el.continent === 'Europe' && el.language === 'JavaScript').length
+}
+
+console.log(countDevelopers([
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+]))
