5c5a85727e57f851377d2d9f48851defa48e7c0b
Denis Wolter <wolter.important@gmail.com>
2025-01-27 12:40:56 +0300

    after review fix

--- task2.js
+++ task2.js
@@ -1,28 +1,38 @@
-WeakMap.prototype.deleteProduct = function (product) {
-    if (this.has(product)) {
-        this.delete(product);
-        console.log(`${product.name} and its discount have been deleted.`);
-    } else {
-        console.log(`${product.name} not found in discounts.`);
-    }
-};
+class ProductsDiscount {
+    static discounts = {}
 
-class Product {
+    setDiscount(discount) {
+        ProductsDiscount.discounts[this.name] = discount;
+    }
+
+    getDiscount() {
+        return ProductsDiscount.discounts[this.name]
+    }
+
+    deleteProduct() {
+        delete ProductsDiscount.discounts[this.name]
+    }
+}
+
+class Product extends ProductsDiscount {
     constructor(name, price) {
+        super()
         this.name = name;
         this.price = price;
     }
 
+    deleteProduct() {
+        super.deleteProduct(this)
+    }
+
     setDiscount(discount) {
-        discounts.set(this, discount);
+        super.setDiscount(discount);
     }
 
     getDiscount() {
-        return discounts.get(this) || "No discount";
+        return super.getDiscount();
     }
 }
-
-const discounts = new WeakMap();
 
 let product1 = new Product("Laptop", 1000);
 let product2 = new Product("Phone", 500);
@@ -30,11 +40,16 @@ let product3 = new Product("Tablet", 300);
 
 product1.setDiscount(10);
 product2.setDiscount(5);
+product3.setDiscount(20);
 
 console.log("Скидка для Laptop:", product1.getDiscount());
 console.log("Скидка для Phone:", product2.getDiscount());
 console.log("Скидка для Tablet:", product3.getDiscount());
 
-discounts.deleteProduct(product2);
+product2.deleteProduct()
 
-console.log("Скидка для Phone после удаления:", discounts.get(product2) || "No discount");
+console.log("Скидка для телефона после удаления:", product2.getDiscount());
+
+// наследование продукта от продуктдискаунта
+// Через обращение к родителю
+// 
